c0468b075ca8586556dfe8fab4af366213f736ca
Godwin Idemudia <godwinide@gmail.com>
2023-01-06 17:53:52 +0100

    new

--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,10 @@ const User = require("../model/User");
 const passport = require("passport");
 const bcrypt = require("bcryptjs");
 
-router.get("/signin", (req,res) => {
-    try{
-        return res.render("trade/signin", {pageTitle: "Login", req, layout: "layout2"});
-    }catch(err){
+router.get("/signin", (req, res) => {
+    try {
+        return res.render("trade/signin", { pageTitle: "Login", req, layout: "layout2" });
+    } catch (err) {
         return res.redirect("/");
     }
 });
@@ -26,56 +26,56 @@ router.get('/logout', (req, res) => {
 });
 
 
-router.get("/signup", (req,res) => {
-    try{
-        return res.render("trade/signup", {pageTitle: "Signup", req, layout: "layout2"});
-    }catch(err){
+router.get("/signup", (req, res) => {
+    try {
+        return res.render("trade/signup", { pageTitle: "Signup", req, layout: "layout2" });
+    } catch (err) {
         return res.redirect("/");
     }
 });
 
 
-router.post('/signup', async (req,res) => {
-    try{
+router.post('/signup', async (req, res) => {
+    try {
         const {
-            firstname, 
-            lastname, 
-            email, 
-            phone, 
-            country, 
-            zip, 
+            firstname,
+            lastname,
+            email,
+            phone,
+            country,
+            zip,
             currency,
             investment_options,
             investment_plans,
-            product_type, 
-            password, 
-            password2   
+            product_type,
+            password,
+            password2
         } = req.body;
-        const user = await User.findOne({email});
-        if(user){
-            return res.render("signup", {...req.body,error_msg:"A User with that email or email already exists", pageTitle: "Signup"});
-        } else{
-            if(!firstname || !lastname || !country || !investment_options || !investment_plans || !product_type || !email || !phone || !password || !password2){
-                return res.render("trade/signup", {...req.body,error_msg:"Please fill all fields", pageTitle: "Signup"});
-            }else{
-                if(password !== password2){
-                    return res.render("trade/signup", {...req.body,error_msg:"Both passwords are not thesame", pageTitle: "Signup"});
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.render("trade/signup", { ...req.body, error_msg: "A User with that email or email already exists", pageTitle: "Signup" });
+        } else {
+            if (!firstname || !lastname || !country || !investment_options || !investment_plans || !product_type || !email || !phone || !password || !password2) {
+                return res.render("trade/signup", { ...req.body, error_msg: "Please fill all fields", pageTitle: "Signup" });
+            } else {
+                if (password !== password2) {
+                    return res.render("trade/signup", { ...req.body, error_msg: "Both passwords are not thesame", pageTitle: "Signup" });
                 }
-                if(password2.length < 6 ){
-                    return res.render("trade/signup", {...req.body,error_msg:"Password length should be min of 6 chars", pageTitle: "Signup"});
+                if (password2.length < 6) {
+                    return res.render("trade/signup", { ...req.body, error_msg: "Password length should be min of 6 chars", pageTitle: "Signup" });
                 }
-                
+
                 const newUser = {
                     firstname,
                     lastname,
                     email,
                     phone,
                     country: country[0],
-                    zip_code: zip, 
+                    zip_code: zip,
                     currency,
                     investment_options,
                     investment_plans,
-                    product_type, 
+                    product_type,
                     password
                 };
                 const salt = await bcrypt.genSalt();
@@ -87,7 +87,7 @@ router.post('/signup', async (req,res) => {
                 return res.redirect("/trade/signin");
             }
         }
-    }catch(err){
+    } catch (err) {
         console.log(err)
     }
 })
